c195e94e17955f98de7c99dcd4f60d66316c0a15
Ridanis <khaled.chikhou@gmail.com>
2023-01-14 21:40:34 +0100

    1.0.0 Initial setup for Ars Nouveau Addon

--- src/main/java/com/ridanisaurus/eearsnouveauaddon/EEArsNouveauAddon.java
+++ src/main/java/com/ridanisaurus/eearsnouveauaddon/EEArsNouveauAddon.java
@@ -5,7 +5,7 @@ import net.minecraftforge.fml.common.Mod;
 @Mod(EEArsNouveauAddon.MOD_ID)
 public class EEArsNouveauAddon
 {
-    public static final String MOD_ID = "eearsnouveauaddon";
+    public static final String MOD_ID = "ee_ars_nouveau_addon";
     public static final String ARSNOUVEAU = "ars_nouveau";
 
     public EEArsNouveauAddon()
